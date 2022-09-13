7ed27d96b72ca8f5273363a33c3e97853e8d16bb
valyndsilva <valyndsilva@gmail.com>
2022-09-13 13:26:56 +0100

    ts config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    domains: ["images.unsplash.com","lh3.googleusercontent.com"],
+    domains: ["images.unsplash.com", "lh3.googleusercontent.com"],
   },
 };
